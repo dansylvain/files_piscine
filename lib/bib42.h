101197381af1cc6cf17e25eab04b8ce951518102
Dan Sylvain <dsylvain@c3r1p3.42perpignan.fr>
2023-07-24 19:17:44 +0200

    initial commit

---
+++ lib/bib42.h
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_print_comb(void);
+void	ft_ft(int *nbr);
+void	ft_print_numbers(void);
+void	ft_print_alphabet(void);
+void	ft_is_negative(int n);
+void	ft_div_mod(int a, int b, int *div, int *mod);
+void	ft_print_char(int num1, int num2, int num3);
+void	ft_print_alphabet(void);
+void	ft_is_negative(int n);
+void	ft_print_reverse_alphabet(void);
+void	ft_putchar(char c);
+void	ft_print_numbers(void);
+void	ft_ultimate_ft(int *********nbr);
+void	ft_print_reverse_alphabet(void);
+void	ft_putstr(char *str);
+int	ft_strlen(char *str);
+void	ft_ultimate_div_mod(int *a, int *b);
+void	ft_swap(int *a, int *b);
+int	ft_strcmp(char *s1, char *s2);
+int	ft_strncmp(char *s1, char *s2, unsigned int n);
+char	*ft_strcat(char *dest, char *src);
+char	*ft_strncat(char *dest, char *src, unsigned int nb);
+char	*ft_strstr(char *str, char *to_find);
+
+
+
+
